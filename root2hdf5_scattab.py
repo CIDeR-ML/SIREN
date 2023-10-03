fb3d82d0a9c1d25c71327499937a0b7e1a8ff0dd
Patrick de Perio <pdeperio@ipmu.jp>
2023-10-03 15:03:52 +0900

    Update actual paths used on gpgpu: /home/pdeperio/wcte/ryom/fiTQun_tuning/TuningFiles/scattable

--- root2hdf5_scattab.py
+++ root2hdf5_scattab.py
@@ -2,8 +2,8 @@ import uproot
 import numpy as np
 import h5py as h5
 
-fname_in='/home/drinkingkazu/Downloads/fiTQun_scattablesF_nuPRISMBeamTest_16cShort_mPMT.root'
-fname_out='myass.h5'
+fname_in='/home/pdeperio/wcte/ryom/fiTQun_tuning/TuningFiles/scattable/fiTQun_scattablesF_nuPRISMBeamTest_16cShort_mPMT.root'
+fname_out='myassfiTQun_scattablesF_nuPRISMBeamTest_16cShort_mPMT.h5'
 
 with uproot.open(fname_in) as fin:
     print('Objects:',fin.keys(),'\n')
